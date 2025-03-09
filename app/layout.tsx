82119028209fade20fd232851e6e3b3eeb3c92d9
NicoPli <n@pliquett.net>
2025-03-09 13:17:33 +0100

    Added Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react"
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { LanguageProvider } from '../contexts/LanguageContext';
@@ -31,6 +32,7 @@ export default function RootLayout({
         <LanguageProvider>
           {children}
         </LanguageProvider>
+        <Analytics />
       </body>
     </html>
   );
